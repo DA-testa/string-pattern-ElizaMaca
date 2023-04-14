e0b1b4f9b6d1c678254ce6abc2c3ffbe89990368
ElizaMaca <124578737+ElizaMaca@users.noreply.github.com>
2023-04-14 12:26:38 +0000

    FinalCheck

--- hash_substring.py
+++ hash_substring.py
@@ -3,6 +3,7 @@
 def read_input():
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
+    
     input_type=input().rstrip().lower()
     
     if input_type == 'i':
@@ -12,13 +13,6 @@ def read_input():
             return (f.readline().rstrip(), f.readline().rstrip())
     else:
         raise ValueError('Invalid input type')
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    # return both lines in one return
-    # this is the sample return, notice the rstrip function
-    # return (input().rstrip(), input().rstrip())
 
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
@@ -30,17 +24,15 @@ def get_occurrences(pattern, text):
     text_length=len(text)
     pattern_hash=hash(pattern)
     text_hash=hash(text[:pattern_length])
-    occurrences=[]
+    occurrances=[]
 
     for i in range(text_length-pattern_length+1):
         if pattern_hash==text_hash and pattern==text[i:i+pattern_length]:
-            occurrences.append(i)
+            occurrances.append(i)
         if i<text_length-pattern_length:
             text_hash=hash(text[i+1:i+pattern_length+1])
     # and return an iterable variable
-    return occurrences
+    return occurrances
 
-
-# this part launches the functions
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
