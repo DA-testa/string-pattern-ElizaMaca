8d4dec44ace4427068301b237e239aff3b6aa57e
ElizaMaca <124578737+ElizaMaca@users.noreply.github.com>
2023-04-14 12:08:56 +0000

    fourthCheck

--- hash_substring.py
+++ hash_substring.py
@@ -3,17 +3,16 @@
 def read_input():
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    # input_type=input().rstrip()
-    pattern=input().rstrip()
-    text=input().rstrip()
-    return pattern, text
-    # if input_type == 'i':
-    #     return (input().rstrip(), input().rstrip())
-    # elif input_type == 'f':
-    #     with open(input().rstrip(), 'r') as f:
-    #         return (f.readline().rstrip(), f.readline().rstrip())
-    # else:
-    #     raise ValueError('Invalid input type')
+    input_type=input().rstrip().lower()
+    # pattern=input().rstrip()
+    # text=input().rstrip()
+    if input_type == 'i':
+        return (input().rstrip(), input().rstrip())
+    elif input_type == 'f':
+        with open(input().rstrip(), 'r') as f:
+            return (f.readline().rstrip(), f.readline().rstrip())
+    else:
+        raise ValueError('Invalid input type')
     # after input type choice
     # read two lines 
     # first line is pattern 
@@ -39,8 +38,6 @@ def get_occurrences(pattern, text):
             occurrences.append(i)
         if i<text_length-pattern_length:
             text_hash=hash(text[i+1:i+pattern_length+1])
-
-
     # and return an iterable variable
     return occurrences
 
@@ -48,4 +45,3 @@ def get_occurrences(pattern, text):
 # this part launches the functions
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
-
